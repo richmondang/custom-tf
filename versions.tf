af803e2e6a61ce4810c3c0269d17f954c854aa03
richmondang <richmondang88@gmail.com>
2021-12-02 23:13:35 -0500

    Update tf provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     apex = {
       version = "~> 1.0.0"
-      # source  = "terraform-example.com/exampleprovider/example"
       source = "terraform-apex.com/apexprovider/apex"
     }
   }
